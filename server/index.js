75863ebbd860180cdbcaef31c08086a4dba9ef12
HugoHtvl <hug.oo@hotmail.fr>
2023-07-12 12:23:22 +0200

    initializing API

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.port || 4343;
+
+const app = express();
+
+app.use(cors("*"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome on the O'Sook API !");
+});
+
+const objects = require("./objects.json");
+
+const getObjects = (req, res) => {
+  res.status(200).json(objects);
+};
+
+app.get("/api/objects", getObjects);
+
+app.get("/api/objects/:id", (req, res) => {
+  const element = all.find((element) => element.id == req.params.id);
+  if (element) {
+    res.status(200).json(element);
+  } else {
+    res.status(404).json("There is no object with this id");
+  }
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
